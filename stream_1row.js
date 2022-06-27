6e7b8cdf53b7ae04f86a8b05e13e9a006dba7ea8
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-06-27 12:54:27 +0800

    Update stream_1row.js

--- stream_1row.js
+++ stream_1row.js
@@ -44,7 +44,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
 
   ;(async () => {
     let panel_result = {
-      title: '流媒体解锁检测' + new Date().Format("HH:mm:ss"),
+      title: '📺 流媒体解锁检测 ⏰' + new Date().Format("HH:mm:ss"),
       content: '',
       icon: 'lock.open.display',
       'icon-color': '#FF2D55',
